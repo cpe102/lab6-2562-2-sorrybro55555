c426fc953d932d0bcdba56ae61ec9ac57e6a2568
sorrybro55555 <zom_bie1596@hotmail.com>
2020-01-17 17:10:32 +0700

    Lab6

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,8 +1,45 @@
-//Dialog of the first example is given below.
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+    cout << "Me: ";
+    getline(cin,name);
+    int ans;
+    cout << "Fahsai: Wow!!!" << name <<" "<<"that is a really cool name.\n";
+    cout << "Fahsai: I think you are Engineering student. What is your student ID?\n";
+    cout << name <<": ";
+    cin >> ans;
+    cout << "Fahsai: I think you may be GEAR" << (ans/10000000)-12 << ".I have a free movie tickets for you.\n";
+    cout << "Fahsai: Let's go to cinema together!!!\n";
+    string name2;
+    cout << "Fahsai: What movie do you want to watch?\n";
+    cout << name << ": ";
+    cin >> name2;
+    string text;
+    getline(cin,text);
+    cout << "Fahsai: So....which day are you free to go with me?\n";
+    cout << name <<": ";
+    getline(cin,text);
+    cout << "Fahsai: " << text <<"....that is OK!!! I'm looking forward to watch Star Wars VII with you.\n";
+    cout << name <<": " <<"May the Force be with you krub\n";
+    cout << "Fahsai: 555+ see you " << text <<". Bye Bye \\(^ ^)/ \n";
+
+
+
+
+
+
+
+
+}
 
 /*
 Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
+??????: Luke Skywalker
 Fahsai: Wow!!! Luke Skywalker that is a really cool name.
 Fahsai: I think you are Engineering student. What is your student ID?
 Luke Skywalker: 590610999
--- lab6_3.cpp
+++ lab6_3.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	if(x=='A'){
+		cout << "Z";
+
+	
+	}else{
+		if(x <= 'Z' and x > 'A'){
+			x=(char) x-1;
+			cout << x;
+		}else{
+			cout << 0;
+		}
+	}
+	
 }
 
 int main(){
